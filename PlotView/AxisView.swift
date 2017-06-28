cc06db708db5c8e23c4e64f4e23aef6b8287ec13
James <jbean46@gmail.com>
2017-06-28 17:51:29 +0200

    Make Axis Position initializers public

--- PlotView/AxisView.swift
+++ PlotView/AxisView.swift
@@ -19,9 +19,16 @@ public protocol AxisView: Renderable {
 }
 
 public struct VerticalAxisPosition {
+    
     public let x: Double
     public let plotTop: Double
     public let plotBottom: Double
+    
+    public init(x: Double, plotTop: Double, plotBottom: Double) {
+        self.x = x
+        self.plotTop = plotTop
+        self.plotBottom = plotBottom
+    }
 }
 
 public protocol VerticalAxisView: AxisView {
@@ -29,9 +36,16 @@ public protocol VerticalAxisView: AxisView {
 }
 
 public struct HorizontalAxisPosition {
+    
     public let y: Double
     public let plotLeft: Double
     public let plotRight: Double
+    
+    public init(y: Double, plotLeft: Double, plotRight: Double) {
+        self.y = y
+        self.plotLeft = plotLeft
+        self.plotRight = plotRight
+    }
 }
 
 public protocol HorizontalAxisView: AxisView {
