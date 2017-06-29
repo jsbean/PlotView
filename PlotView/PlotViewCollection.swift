e48483b8975ad3d793774e721301afa2e4345401
James <jbean46@gmail.com>
2017-06-29 21:22:28 +0200

    Add affordances to PlotViewCollections

--- PlotView/PlotViewCollection.swift
+++ PlotView/PlotViewCollection.swift
@@ -6,9 +6,21 @@
 //
 //
 
+import Collections
 import GraphicsTools
 
 public struct PlotViewCollection {
     
     let plots: [PlotView]
+    
+    public init <S: Sequence> (_ plots: S) where S.Iterator.Element == PlotView {
+        self.plots = Array(plots)
+    }
+}
+
+extension PlotViewCollection: AnyCollectionWrapping {
+    
+    public var collection: AnyCollection<PlotView> {
+        return AnyCollection(plots)
+    }
 }
